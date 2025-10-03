205f5938e86db3337f28bd5842ae7f6eb2d5bd38
emanqarooni <emanqaroonibahrain@gmail.com>
2025-10-03 08:04:03 +0300

    finished with the show functionality

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -6,9 +6,16 @@ const multer  = require('multer')
 const upload = multer({ dest: 'uploads/' })
 
 //API's functionality
+
+//show all blogs from all users
+exports.all_blogs_get = async (req, res) => {
+  const blogs = await Blog.find().populate("owner")
+  res.render("blogs/index.ejs", { blogs })
+}
+
+//show only the user's own blogs
 exports.blog_index_get = async (req, res) => {
-  const blog = await Blog.find().populate("owner")
-  res.render("blogs/index.ejs", { blog })
+
 }
 
 exports.blog_create_get = async (req, res) => {
@@ -18,11 +25,11 @@ exports.blog_create_get = async (req, res) => {
 exports.blog_create_post = async (req, res) => {
   req.body.owner = req.session.user._id;
 
-  req.body.images = [];
+  req.body.images = []
   if (req.files["images"]) {
     req.files["images"].forEach((file) => {
       req.body.images.push(file.path)
-    });
+    })
   }
 
   if (req.files["video"]) {
