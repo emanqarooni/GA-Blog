e0bf5911e934ac2e830b3c01856f5f5c06c67ba8
unknown <rabab177hasan@gmail.com>
2025-10-05 11:04:42 +0300

    edit html done

--- controllers/users.js
+++ controllers/users.js
@@ -3,11 +3,19 @@ const User = require("../models/user")
 //showing the user his/her profile
 exports.user_show_get = async (req, res) => {
   const user = await User.findById(req.params.userId)
-  res.render("users/index.ejs", { user })
+  res.render("users/profile.ejs", { user })
 }
 
 //edit profile
 exports.user_edit_get = async (req, res) => {
   const user = await User.findById(req.params.userId)
   res.render("users/edit.ejs", { user })
+}
+
+exports.user_update_put = async (req, res) =>{
+  const user =await User.findById(req.params.userId)
+  user.set(req.body)
+  await user.save()
+  res.redirect(`/users/${req.params.userId}`)
+
 }
--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,7 @@ const router = require("express").Router()
 
 const userCtrl = require("../controllers/users")
 
-router.get('/:userId',userCtrl.user_show_get)
-router.get('/:userId/edit',userCtrl.user_edit_get)
-
-
-
-
-
+router.get("/profile/:userId", userCtrl.user_show_get)
+router.get("/profile/:userId/edit", userCtrl.user_edit_get)
+router.put("/profile/:userId",userCtrl.user_update_put)
 module.exports = router
